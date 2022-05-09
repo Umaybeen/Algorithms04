f0ed97c3eb41c7ffa625aef835a1b6bf982b00af
Marina Shumyater <umaybeen@gmail.com>
2022-05-08 22:05:00 -0700

    first commit

--- src/main/java/AscendingSequence.java
+++ src/main/java/AscendingSequence.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class AscendingSequence {
+public class AscendingSequence {
+    public int[] ascendingSequence(int start, int end, int step) {
+
+        if (step > 0 && start < end) {
+            int[] result = new int[end - start + 1];
+
+            for (int i = 0; i < result.length; i++) {
+                if (start <= end) {
+                    result[i] = start;
+                    start += step;
+
+                }
+
+            }
+
+            return result;
+
+        }
+
+        return new int[]{};
+
+    }
+
 }
+
+
+
